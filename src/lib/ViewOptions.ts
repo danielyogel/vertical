2d561f43b18477505d1c3a21854c425219b7224f
Daniel Yogel <daniel@stoa.fund>
2021-03-18 18:06:37 +0200

    removed unused type

--- src/lib/ViewOptions.ts
+++
@@ -1,8 +0,0 @@
-export interface ViewOptions {
-  width?: 'full' | 'auto' | '1/2' | '1/3' | '1/4';
-  help?: string;
-  label?: string;
-  placeholder?: string;
-  prefix?: string;
-  suffix?: string;
-}

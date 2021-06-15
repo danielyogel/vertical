2b0eb22de30c76592acb44f2032d4f11ac397851
Daniel Yogel <danielyogel@gmail.com>
2021-06-15 19:35:17 +0300

    0.1.3

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,3 +1,4 @@
 export { default as NodeObject } from './nodes/NodeObject';
 export { default as NodeArray } from './nodes/NodeArray';
 export { default as NodeScalar } from './nodes/NodeScalar';
+export * from './mixins';

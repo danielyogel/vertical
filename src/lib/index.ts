e9ce597dd0958da581e8c8e5e3e4e741c6afc993
Daniel Yogel <danielyogel@gmail.com>
2021-09-21 19:12:56 +0300

    0.7.4 - export NodeObject

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -2,6 +2,7 @@ export { default as NodeArrayChildLogical } from './nodes/NodeArrayChildLogical'
 export { default as NodeArray } from './nodes/NodeArray';
 export { default as NodeArrayChild } from './nodes/NodeArrayChild';
 export { default as NodeScalar } from './nodes/NodeScalar';
+export { default as NodeObject } from './nodes/NodeObject';
 export { default as NodeOneOf } from './nodes/NodeOneOf';
 export { default as NodeDynamicArray } from './nodes/NodeDynamicArray';
 export { default as NodeDynamicArrayChild } from './nodes/NodeDynamicArrayChild';

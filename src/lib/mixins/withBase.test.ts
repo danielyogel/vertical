d1eb34ba6fdfc0ee655a38f2df7cd35f250bf60f
Daniel Yogel <danielyogel@gmail.com>
2022-03-29 15:28:04 +0300

    feat(withBase mixin): mixin POC working

--- src/lib/mixins/withBase.test.ts
+++ src/lib/mixins/withBase.test.ts
@@ -5,7 +5,7 @@ import withBase from './withBase';
 test('withBase', () => {
   const store = observable.box({ name: 'john' });
 
-  const INITIAL_VM = { children: null, index: 1, store: computed(() => store.get()), value: computed(() => 1) };
+  const INITIAL_VM = { children: null, index: 1, store: computed(() => store.get()), value: computed(() => 1), onStoreChange: () => {} };
 
   const RESULT = withBase()(INITIAL_VM);
 
@@ -14,5 +14,6 @@ test('withBase', () => {
   expect(RESULT.setLoading).toBeDefined();
   expect(RESULT.label).toBeDefined();
   expect(RESULT.placeholder).toBeDefined();
+  expect(RESULT.isVisible).toBeDefined();
   expect(RESULT.id).toBeDefined();
 });
--- src/lib/mixins/withBase.ts
+++ src/lib/mixins/withBase.ts
@@ -5,14 +5,39 @@ import withId from './withId';
 import withLoading from './withLoading';
 import withErrors, { errors as ErrorsParams } from './withErrors';
 import withDisabled, { isDisabled } from './withDisabled';
+import withVisibility, { isVisible } from './withVisibility';
+import withSelected, { isSelected } from './withSelected';
 
-type Params<V, S, VM extends Pick<Node<V, S>, 'value' | 'store' | 'index' | 'children'>> = MetaParams & {
-  errors: ErrorsParams<V, S, VM & Pick<Node<V, S>, 'id' | 'label' | 'placeholder' | 'isLoading' | 'setLoading'>>;
-  isDisabled?: isDisabled<V, S, VM & Pick<Node<V, S>, 'id' | 'label' | 'placeholder' | 'isLoading' | 'setLoading' | 'errors'>>;
+export type BaseParams<V, S, VM extends Pick<Node<V, S>, 'value' | 'store' | 'index' | 'children'>> = MetaParams & {
+  autoFocus?: boolean;
+  errors?: ErrorsParams<V, S, VM & Pick<Node<V, S>, 'id' | 'label' | 'placeholder' | 'isLoading' | 'setLoading' | 'autoFocus'>>;
+  isDisabled?: isDisabled<V, S, VM & Pick<Node<V, S>, 'id' | 'label' | 'placeholder' | 'isLoading' | 'setLoading' | 'errors' | 'autoFocus'>>;
+  isSelected?: isSelected<
+    V,
+    S,
+    VM & Pick<Node<V, S>, 'id' | 'label' | 'placeholder' | 'isLoading' | 'setLoading' | 'errors' | 'isDisabled' | 'autoFocus'>
+  >;
+  isVisible?: isVisible<
+    V,
+    S,
+    VM & Pick<Node<V, S>, 'id' | 'label' | 'placeholder' | 'isLoading' | 'setLoading' | 'errors' | 'isDisabled' | 'isSelected' | 'autoFocus'>
+  >;
 };
 
-export default function withBase<V, S, VM extends Pick<Node<V, S>, 'value' | 'store' | 'index' | 'children'>>(params?: Params<V, S, VM>) {
+export default function withBase<V, S, VM extends Pick<Node<V, S>, 'value' | 'store' | 'onStoreChange' | 'index' | 'children'>>(
+  params?: BaseParams<V, S, VM>
+) {
   return function (vm: VM) {
-    return pipe(vm, withId(), withMeta(params), withLoading(), withErrors(params?.errors), withDisabled(params?.isDisabled));
+    return pipe(
+      vm,
+      vm => ({ ...vm, autoFocus: params?.autoFocus ?? false }),
+      withId(),
+      withMeta(params),
+      withLoading(),
+      withErrors(params?.errors),
+      withDisabled(params?.isDisabled),
+      withSelected(),
+      withVisibility(params?.isVisible)
+    );
   };
 }

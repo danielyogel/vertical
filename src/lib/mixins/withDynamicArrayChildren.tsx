fff5f4581eb7a9cceeab11187359d8106e80c4dc
Daniel Yogel <danielyogel@gmail.com>
2023-09-26 20:13:01 +0300

    refactored withDynamicArrayChildren

--- src/lib/mixins/withDynamicArrayChildren.tsx
+++ src/lib/mixins/withDynamicArrayChildren.tsx
@@ -1,40 +1,84 @@
 import { O } from 'ts-toolbelt';
-import { computed, observable, reaction } from 'mobx';
+import { IObservableValue, computed, observable, reaction } from 'mobx';
 import { DynamicArrayChildNode, DynamicArrayNode, Node } from '../Interfaces';
 import { isEqual, pipe, unsafeUpdateAt } from '../utils';
 import { nanoid } from 'nanoid';
 
 type PreviusVM<V, S> = Pick<DynamicArrayNode<V, S>, 'value' | 'store' | 'onChange' | 'onStoreChange'>;
 
+type VmWithChildren<V extends Record<string, any>, S> = Pick<
+  DynamicArrayChildNode<Omit<V, 'id'>, S>,
+  'onChange' | 'onStoreChange' | 'store' | 'value' | 'index' | 'remove' | 'setSelectedId'
+>;
+
+type childVM<V extends Record<string, any>, S> = O.Required<Partial<DynamicArrayChildNode<V, S>>, 'View' | 'id'>;
+
 export type Params<V extends Record<string, any>, S> = {
-  child: (
-    params: Pick<DynamicArrayChildNode<Omit<V, 'id'>, S>, 'onChange' | 'onStoreChange' | 'store' | 'value' | 'index' | 'remove' | 'setSelectedId'>
-  ) => O.Required<Partial<DynamicArrayChildNode<V, S>>, 'View' | 'id'>;
-  childEdit?: (
-    params: Pick<DynamicArrayChildNode<Omit<V, 'id'>, S>, 'onChange' | 'onStoreChange' | 'store' | 'value' | 'index' | 'remove' | 'setSelectedId'>
-  ) => O.Required<Partial<DynamicArrayChildNode<V, S>>, 'View' | 'id'>;
+  child: (params: VmWithChildren<V, S>) => childVM<V, S>;
+  childEdit?: (params: VmWithChildren<V, S>) => childVM<V, S>;
   defaultValue: Omit<V, 'id'>;
   selectedId?: { get: () => string | null; set: (id: string | null) => void };
 };
 
 export default function withDynamicArrayChildren<V extends { id: string }, S, VM extends PreviusVM<V, S>>(options: Params<V, S>) {
   return function (vm: VM) {
-    const _children = observable.box<Array<ReturnType<(typeof options)['child']>>>([]);
+    type ChildVmType = ReturnType<(typeof options)['child']>;
 
-    const _selectedId = options.selectedId || observable.box<null | string>(null);
+    const children: IObservableValue<ChildVmType[]> = observable.box([]);
 
-    const _selectedChild = observable.box<null | ReturnType<(typeof options)['child']>>(null);
+    const _selectedId: IObservableValue<null | string> = options.selectedId || observable.box(null);
 
+    const add = () => {
+      const newItem = { ...options.defaultValue, id: nanoid() } as V;
+      vm.onChange([newItem, ...vm.value.get()]);
+    };
+
+    const removeById = (id: string) => {
+      const childReadID = children
+        .get()
+        .find(c => c.id === id)
+        ?.value?.get().id;
+
+      children.set(children.get().filter(c => c.id !== id));
+      vm.onChange(vm.value.get().filter(v => v.id !== childReadID));
+    };
+
+    const selectById = (id: string | null) => {
+      _selectedId.set(
+        children
+          .get()
+          .find(c => c.id === id)
+          ?.value?.get().id || null
+      );
+    };
+
+    const _selectedChild = observable.box<ChildVmType | null>(null);
+
+    const selectedId = {
+      get: () => _selectedId.get(),
+      set: selectById
+    };
+
+    const selectedChild = computed(() => _selectedChild.get());
+
+    const allowChildEdit = Boolean(options.childEdit);
+
+    //
+    // reaction for syncing outside value
+    //
     reaction(
       () => vm.value.get(),
       items => {
-        let newChildren: Array<ReturnType<(typeof options)['child']>> = [];
+        let newChildren: ChildVmType[] = [];
         items.forEach((item, index) => {
-          const indexTouse = () => vm.value.get().findIndex(i => i.id === item.id);
+          const VM = children.get().find(c => c.value?.get().id === item.id);
 
-          const VM =
-            _children.get().find(c => c.value?.get().id === item.id) ??
-            options.child({
+          if (VM) {
+            newChildren[index] = VM;
+          } else {
+            const indexTouse = () => vm.value.get().findIndex(i => i.id === item.id);
+
+            newChildren[index] = options.child({
               index: 0,
               setSelectedId: id => _selectedId.set(id),
               value: computed(() => {
@@ -44,17 +88,16 @@ export default function withDynamicArrayChildren<V extends { id: string }, S, VM
                 return pipe(unsafeUpdateAt(indexTouse(), { ...vm.value.get()[index], ...v }, vm.value.get()), vm.onChange);
               },
               remove: () => {
-                _children.set(_children.get().filter(c => c.value?.get().id !== item.id));
+                children.set(children.get().filter(c => c.value?.get().id !== item.id));
                 vm.onChange(vm.value.get().filter(v => v.id !== item.id));
               },
               onStoreChange: vm.onStoreChange,
               store: vm.store
             });
-
-          newChildren[index] = VM;
+          }
         });
 
-        _children.set(newChildren);
+        children.set(newChildren);
       },
       {
         fireImmediately: true,
@@ -66,23 +109,26 @@ export default function withDynamicArrayChildren<V extends { id: string }, S, VM
       }
     );
 
+    //
+    // reaction for initializing a new editVM when selectedId changes
+    //
     reaction(
       () => _selectedId.get(),
       id => {
-        const newLocal = options.childEdit;
+        const childEditConstructor = options.childEdit;
 
-        const realId = _children
+        const realId = children
           .get()
           .find(vm => vm.value?.get().id === id)
           ?.value?.get().id;
 
-        if (!realId || !id || !newLocal) {
+        if (!realId || !id || !childEditConstructor) {
           return _selectedChild.set(null);
         }
 
         const indexTouse = () => vm.value.get().findIndex(i => i.id === realId);
 
-        const _vm = newLocal({
+        const VM = childEditConstructor({
           index: 0,
           setSelectedId: id => _selectedId.set(id),
           value: computed(() => {
@@ -92,7 +138,7 @@ export default function withDynamicArrayChildren<V extends { id: string }, S, VM
             return pipe(unsafeUpdateAt(indexTouse(), { ...vm.value.get()[indexTouse()], ...v }, vm.value.get()), vm.onChange);
           },
           remove: () => {
-            _children.set(_children.get().filter(c => c.value?.get().id !== realId));
+            children.set(children.get().filter(c => c.value?.get().id !== realId));
             _selectedChild.set(null);
             vm.onChange(vm.value.get().filter(v => v.id !== realId));
           },
@@ -100,45 +146,19 @@ export default function withDynamicArrayChildren<V extends { id: string }, S, VM
           store: vm.store
         });
 
-        _selectedChild.set(_vm);
+        _selectedChild.set(VM);
       },
       { fireImmediately: true }
     );
 
-    const _add = () => {
-      const newItem = { ...options.defaultValue, id: nanoid() } as V;
-      vm.onChange([newItem, ...vm.value.get()]);
-    };
-
-    const _removeById = (id: string) => {
-      const childReadID = _children
-        .get()
-        .find(c => c.id === id)
-        ?.value?.get().id;
-      _children.set(_children.get().filter(c => c.id !== id));
-      vm.onChange(vm.value.get().filter(v => v.id !== childReadID));
-    };
-
     return {
       ...vm,
-      selectedChild: computed(() => _selectedChild.get()),
-      add: _add,
-      removeById: _removeById,
-      allowChildEdit: !!options.childEdit,
-      children: _children,
-      selectedId: {
-        get() {
-          return _selectedId.get();
-        },
-        set(id: string | null) {
-          return _selectedId.set(
-            _children
-              .get()
-              .find(c => c.id === id)
-              ?.value?.get().id || null
-          );
-        }
-      }
+      selectedChild,
+      add,
+      removeById,
+      allowChildEdit,
+      children,
+      selectedId
     };
   };
 }
